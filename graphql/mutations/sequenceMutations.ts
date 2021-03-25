ea491f9ce380e41be58c7798829a6caaa3ec4090
Adam Berkowitz <adam@adamjberkowitz.com>
2021-03-25 17:42:02 -0400

    working on creating sequences from techniques

--- graphql/mutations/sequenceMutations.ts
+++ graphql/mutations/sequenceMutations.ts
@@ -3,16 +3,53 @@ import slugify from '@sindresorhus/slugify'
 
 import connectToUser from '../../lib/connectToUser'
 import { Context } from '../../pages/api/graphql'
+import prisma from '../../prisma/prisma'
 
 export const sequenceMutations = {
   createSequence: async (_, { sequenceData }, ctx: Context) => {
-    const { user } = ctx.verifiedUser
+    const connectedUser = connectToUser(ctx.verifiedUser)
 
-    if (!user) throw new Error('Not authenticated')
+    const uuid = uuidv4()
+    const slug = `${slugify(sequenceData.name)}-${uuid.substr(0, 8)}`
 
     const techniqueIDs = sequenceData.techniques.map(({ uuid }) => uuid)
 
-    // const createdTechniqueOrder = sequenceData.techniqueOrder.map()
+    const createdTechniqueOrder = sequenceData.techniqueOrder.map(({ techniqueID }) => {
+      return {
+        "techniqueId": parseInt(techniqueID)
+      }
+    })
+
+    const techniques = await prisma.technique.findMany({
+      where: {
+        uuid: {
+          in: techniqueIDs
+        }
+      }
+    })
+
+    const createdSequence = await prisma.sequence.create({
+      data: {
+        creator: {
+          connect: connectedUser
+        },
+        uuid,
+        slug,
+        name: sequenceData.name,
+        description: sequenceData.description,
+        techniqueOrder: {
+          "order": createdTechniqueOrder
+        },
+      },
+      include: {
+        creator: true
+      }
+    })
+
+
+    createdSequence.techniques = techniques
+
+    return createdSequence
   },
   updateSequence: async (_, { sequenceData }, ctx: Context) => {
 
